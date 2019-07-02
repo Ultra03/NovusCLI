6059ea5b12949d885cba350e7ce4bb1c1bceae58
Diego Magdaleno <diegomagdaleno@protonmail.com>
2019-07-01 19:55:39 -0500

    Release is gone

--- nvs/target/release/deps/nvs-9cc15a6924f227d3.d
+++
@@ -1,5 +0,0 @@
-/Users/SmushyTaco/Downloads/NovusCLI-master/nvs/target/release/deps/nvs-9cc15a6924f227d3: src/main.rs
-
-/Users/SmushyTaco/Downloads/NovusCLI-master/nvs/target/release/deps/nvs-9cc15a6924f227d3.d: src/main.rs
-
-src/main.rs:
--- nvs/target/release/nvs.d
+++
@@ -1,1 +0,0 @@
-/Users/SmushyTaco/Downloads/NovusCLI-master/nvs/target/release/nvs.dSYM: /Users/SmushyTaco/Downloads/NovusCLI-master/nvs/src/main.rs
