25d968d8a00faef88c0e59620f19400835863bfa
SmushyTaco <41648290+realSmushyTaco@users.noreply.github.com>
2019-07-01 17:15:47 -0700

    Info error message fix

--- nvs/src/main.rs
+++ nvs/src/main.rs
@@ -44,7 +44,7 @@ fn main() {
 			println!("About Novus CLI\nCopyright (C) 2019 Polar Development.\nhttp://randomlink.com\n\nCopyright 2019 Polar Team\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License./nYou may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n\nNovus CLI is made possible by the following software and people: \n\nNikan Radan (SmushyTaco) for writing it\nAPT by Debian: https://packages.debian.org/stretch/apt\nDiego Magdaleno & Diatrus for hosting it on Project Serna: https://sernarepo.com/");
 			process::exit(0);
 			//Checks if any of the entries with 2 arguments were entered with only 1 argument.
-		} else if args[1] == "search" || args[1] == "install" || args[1] == "reinstall" || args[1] == "remove" || args[1] == "edit-sources" {
+		} else if args[1] == "search" || args[1] == "install" || args[1] == "reinstall" || args[1] == "remove" || args[1] == "edit-sources" || args[1] == "info" {
 			eprintln!("{:?} has two arguments but you only entered one, run \"nvs help\" to check how to properly use {:?}.", args[1], args[1]);
 
 			process::exit(1);
